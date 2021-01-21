317d6e2a48fd22ee985e58c4c1332f62afc9835d
Bruno Steinmann <bruno.steinmann@gmx.com>
2021-01-21 17:45:54 +0100

    added horizontal navbar and mobile first design

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,64 @@
 <template>
   <v-app>
-    <h1>App</h1>
-    <router-view />
+    <v-app-bar qpp fixed color="primary" light>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="hidden-xs-only">
+        <router-link to="/" tag="span" style="cursor: pointer"
+          >VueShare</router-link
+        >
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-text-field
+        flex
+        prepend-icon="mdi-magnify"
+        placeholder="Search Posts"
+        color="secondary"
+        single-line
+        hide-details
+      ></v-text-field>
+
+      <v-spacer></v-spacer>
+
+      <!-- Horizontal Navbar Links -->
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          text
+          v-for="item in horizontalNavItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-icon class="hidden-sm-only" left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <main>
+      <v-container class="mt-15">
+        <router-view />
+      </v-container>
+    </main>
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
+  computed: {
+    horizontalNavItems() {
+      return [
+        { icon: "mdi-message-text", title: "Posts", link: "/posts" },
+        { icon: "mdi-lock-open", title: "Sign In", link: "/signin" },
+        { icon: "mdi-pencil", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  },
 
   components: {},
 
