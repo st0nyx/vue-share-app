0cac50badec2cc3bf26ff4f5e244528bf008d9c7
Bruno Steinmann <bruno.steinmann@gmx.com>
2021-01-22 15:48:16 +0100

    refactoring vuex queries

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { gql } from "apollo-boost";
+// import { gql } from "apollo-boost";
 import { defaultClient as apolloClient } from "@/main";
+import { GET_POSTS } from "@/queries";
 
 Vue.use(Vuex);
 
@@ -24,15 +25,7 @@ export default new Vuex.Store({
       // use ApolloClient to fire getPosts query
       apolloClient
         .query({
-          query: gql`
-            query {
-              getPosts {
-                _id
-                title
-                imageUrl
-              }
-            }
-          `
+          query: GET_POSTS
         })
         .then(({ data }) => {
           commit("setPosts", data.getPosts);
