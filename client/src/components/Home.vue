e4f7143636a228c1c198384f22836bc7803f65db
Bruno Steinmann <bruno.steinmann@gmx.com>
2021-01-22 15:01:14 +0100

    firing getPosts action with vuex

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,37 +1,30 @@
 <template>
-  <v-container text-justify v-if="getPosts">
-    <v-flex xs12>
-      <v-carousel v-bind="{ cycle: true }" interval="3000">
-        <v-carousel-item
-          v-for="post in getPosts"
-          :key="post._id"
-          :src="post.imageUrl"
-        >
-          <h1 id="carousel__title">{{ post.title }}</h1>
-        </v-carousel-item>
-      </v-carousel>
-    </v-flex>
+  <v-container text-justify>
+    <!--    <v-flex xs12>-->
+    <!--      <v-carousel v-bind="{ cycle: true }" interval="3000">-->
+    <!--        <v-carousel-item-->
+    <!--          v-for="post in getPosts"-->
+    <!--          :key="post._id"-->
+    <!--          :src="post.imageUrl"-->
+    <!--        >-->
+    <!--          <h1 id="carousel__title">{{ post.title }}</h1>-->
+    <!--        </v-carousel-item>-->
+    <!--      </v-carousel>-->
+    <!--    </v-flex>-->
   </v-container>
 </template>
 
 <script>
-import { gql } from "apollo-boost";
+// import { gql } from "apollo-boost";
 
 export default {
   name: "home",
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            description
-            likes
-          }
-        }
-      `
+  created() {
+    this.handleGetCarouselPosts();
+  },
+  methods: {
+    handleGetCarouselPosts() {
+      this.$store.dispatch("getPosts");
     }
   }
 };
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { gql } from "apollo-boost";
+import { defaultClient as apolloClient } from "@/main";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
-  modules: {}
+  actions: {
+    getPosts: () => {
+      // use ApolloClient to fire getPosts query
+      apolloClient
+        .query({
+          query: gql`
+            query {
+              getPosts {
+                _id
+                title
+                imageUrl
+              }
+            }
+          `
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
 });
